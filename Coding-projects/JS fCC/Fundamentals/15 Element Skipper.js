eaf55a60d30e6db7021105f023cd15418035c26d
Cedric <135468727+Mboi64@users.noreply.github.com>
2026-05-20 01:45:58 -0400

    Create 15 Element Skipper.js

---
+++ Coding-projects/JS fCC/Fundamentals/15 Element Skipper.js
@@ -0,0 +1,50 @@
+// function that accepts an array (arr) and a function (func) as arguments.
+
+const dropElements = (arr, func) => {
+  let result = Array.from(arr.length);
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) {
+      for (let j = i; j < arr.length; j++) {
+        result.push(arr[j]);
+      }
+      i += 2;
+    }
+  }
+  return result;
+};
+
+console.log(
+  dropElements([1, 2, 3, 4], function (n) {
+    return n >= 3;
+  }),
+);
+
+console.log(
+  dropElements([1, 2, 3], function (n) {
+    return n > 0;
+  }),
+);
+
+console.log(
+  dropElements([0, 1, 0, 1], function (n) {
+    return n === 1;
+  }),
+);
+
+console.log(
+  dropElements([1, 2, 3, 9, 2], function (n) {
+    return n > 2;
+  }),
+);
+
+console.log(
+  dropElements([1, 2, 3, 7, 4], function (n) {
+    return n > 3;
+  }),
+);
+
+console.log(
+  dropElements([1, 2, 3, 4], function (n) {
+    return n > 5;
+  }),
+);
