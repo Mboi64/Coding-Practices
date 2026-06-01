ee2aa5f439c3317e66cc86d7733cc2459d7d3e43
Cedric <135468727+Mboi64@users.noreply.github.com>
2026-05-31 23:27:28 -0400

    Create 7 Range-Based LCM.js

---
+++ Coding-projects/JS fCC/Higher Order Functions/7 Range-Based LCM.js
@@ -0,0 +1,81 @@
+// Return number should be LCM for all the numbers from the range given in the array
+
+function smallestCommons(arr) {
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  let range = [];
+
+  for (let i = min; i <= max; i++) {
+    range.push(i);
+  }
+
+  let value = range.reduce(
+    (result, currentValue) => lcm(result, currentValue),
+    1,
+  );
+
+  return value;
+}
+
+console.log(smallestCommons([1, 5]));
+console.log(smallestCommons([2, 10]));
+console.log(smallestCommons([23, 18]));
+
+// Borrowed LCM (Lowest Common Multiple) and GCM (Greatest Common Multiple) algorithm
+function gcd(a, b) {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return a;
+}
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
+}
+
+
+
+
+
+/*
+ORIGINAL SOLUTION
+Main issues: 
+  - it takes a long time for numbers or range greater than 10 
+  - the program is very cluttered
+*/
+
+// Least Common Multiple Algorithm for 2 values in an array
+// function lowestCommonMultiple(a, b) {
+//   const max = Math.max(a, b);
+//   const min = Math.min(a, b);
+
+//   let factors = [];
+
+//   for (let i = 1; i <= max; i++) {
+//     factors.push(i);
+//   }
+
+//   const maxMultiples = factors.map((num) => num * max);
+//   const minMultiples = factors.map((num) => num * min);
+//   const filteredMaxMultiples = maxMultiples.filter((maxNum) =>
+//     minMultiples.some((minNum) => maxNum === minNum),
+//   );
+
+//   return Math.min(...filteredMaxMultiples);
+// }
+
+// function smallestCommons(arr){
+//   const max = Math.max(...arr);
+//   const min = Math.min(...arr);
+//   let range = []
+
+//   for (let i = min; i <= max; i++) {
+//     range.push(i);
+//   }
+
+//   let currentValue = lowestCommonMultiple(range[0], range[1]);
+//   for (let i = 2; i < range.length; i++){
+//     currentValue = lowestCommonMultiple(currentValue, range[i])
+//   }
+
+//   return currentValue
+// }
