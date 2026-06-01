7fbba039d883bf68386352f8f26de13e50185430
Cedric <135468727+Mboi64@users.noreply.github.com>
2026-06-01 01:18:16 -0400

    Create 9 All-True Propety Validator.js

---
+++ Coding-projects/JS fCC/Higher Order Functions/9 All-True Propety Validator.js
@@ -0,0 +1,94 @@
+// If the property with the name equal to the second argument has a truthy value in all the objects of the array, and return true if it has, and false otherwise.
+
+function truthCheck(collection, pre) {
+  const result = collection.every((obj) => {
+    if (obj[pre]) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return result;
+}
+
+console.log(
+  truthCheck(
+    [
+      { name: "Quincy", role: "Founder", isBot: false },
+      { name: "Naomi", role: "", isBot: false },
+      { name: "Camperbot", role: "Bot", isBot: true },
+    ],
+    "isBot",
+  ),
+);
+
+console.log(
+  truthCheck(
+    [
+      { name: "Quincy", role: "Founder", isBot: false },
+      { name: "Naomi", role: "", isBot: false },
+      { name: "Camperbot", role: "Bot", isBot: true },
+    ],
+    "name",
+  ),
+);
+
+console.log(
+  truthCheck(
+    [
+      { name: "Quincy", role: "Founder", isBot: false },
+      { name: "Naomi", role: "", isBot: false },
+      { name: "Camperbot", role: "Bot", isBot: true },
+    ],
+    "role",
+  ),
+);
+
+console.log(
+  truthCheck(
+    [
+      { name: "Pikachu", number: 25, caught: 3 },
+      { name: "Togepi", number: 175, caught: 1 },
+    ],
+    "number",
+  ),
+);
+
+console.log(
+  truthCheck(
+    [
+      { name: "Pikachu", number: 25, caught: 3 },
+      { name: "Togepi", number: 175, caught: 1 },
+      { name: "MissingNo", number: NaN, caught: 0 },
+    ],
+    "caught",
+  ),
+);
+
+console.log(
+  truthCheck(
+    [
+      { name: "freeCodeCamp", users: [{ name: "Quincy" }, { name: "Naomi" }] },
+      { name: "Code Radio", users: [{ name: "Camperbot" }] },
+      { name: "", users: [] },
+    ],
+    "users",
+  ),
+);
+
+console.log(
+  truthCheck(
+    [
+      {
+        id: 1,
+        data: { url: "https://freecodecamp.org", name: "freeCodeCamp" },
+      },
+      {
+        id: 2,
+        data: { url: "https://coderadio.freecodecamp.org/", name: "CodeRadio" },
+      },
+      { id: null, data: {} },
+    ],
+    "id",
+  ),
+);
