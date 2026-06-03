18650fda5cb7b7b88d84617afa2157c304d37783
Cedric <135468727+Mboi64@users.noreply.github.com>
2026-06-03 19:05:09 -0400

    Notes and readjustments

--- Coding-projects/JS fCC/DOM Manipulation/1 Favorite Icon Toggler/script.js
+++ Coding-projects/JS fCC/DOM Manipulation/1 Favorite Icon Toggler/script.js
@@ -1,7 +1,16 @@
 const heart = document.querySelectorAll(".favorite-icon");
+// querySelectorAll() shows a Node list which is an object containing every class, id, or tag from first to final apperance
+// Completely different from querySelector, which shows the first instance of the specfic class or id
 
+/* 
+Since accessing a Node List works similarly to how you would acces an array you can use:
+    - Bracket Notations (to find a specific item)
+    - For or For...of loop (to loop through each item)
+    - Higher Order Function/ CallBack Methods like .map() or .filter()
+*/
 heart.forEach((btn) => {
   btn.addEventListener("click", () => {
+    // when clicked this class will be added/removed, changing the icon's image
     btn.classList.toggle("filled");
     if (btn.classList.contains("filled")) {
       btn.innerHTML = "&#10084;";
