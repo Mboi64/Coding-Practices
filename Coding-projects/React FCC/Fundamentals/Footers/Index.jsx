556dc84b7de2b03691dab65aac17912416b7be2c
Cedric <135468727+Mboi64@users.noreply.github.com>
2026-06-28 21:44:21 -0400

    Creating a Footer
    I will try to learn alittle bit of React while learning JS

---
+++ Coding-projects/React FCC/Fundamentals/Footers/Index.jsx
@@ -0,0 +1,30 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <ul className="link">
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Discord</a></li>
+        <li><a href="#">Email</a></li>
+      </ul>
+      <ul className="link">
+        <li><a href="#">Service</a></li>
+        <li><a href="#">Watch Videos</a></li>
+        <li><a href="#">Terms & Conditions</a></li>
+      </ul>
+      <p>© 2024 Fitness Dashboard. All Rights Reserved</p>
+      <ul className="link">
+        <li><a href="#">🎮</a></li>
+        <li><a href="#">🐦</a></li>
+        <li><a href="#">🏀</a></li>
+      </ul>
+    </footer>
+  )
+}
+
+{/* Comments works similarly to JS but with brackets around it */}
+{/* React uses "compnents":
+  Functional - works similarly as JS functions 
+  Class-Based - works similarly as JS Classes
+  */}
+{/* Import and export also works similarly to JS */}
+
